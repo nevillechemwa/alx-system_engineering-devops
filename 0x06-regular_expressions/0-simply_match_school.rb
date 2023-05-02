b757d9c83f8382a1dff02e3373292a98e9d1d540
nevillechemwa <nevillechemwa@gmail.com>
2023-05-02 10:33:48 -0700

    Simply matching School

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+#Ruby script that accepts one argument and pass it to a regular expression
+# matching method
+#Regular expression must match school
+
+puts ARGV[0].scan(/School/).join
